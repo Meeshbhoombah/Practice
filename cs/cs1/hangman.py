7a7666ef4b90ba7290d763ac8fa0826bad2055d6
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-24 22:42:15 -0500

    Added ASCII art for Hangman

--- cs/cs1/hangman.py
+++ cs/cs1/hangman.py
@@ -1,6 +1,6 @@
-# Created by meeshbhoombah
-
 """
+Created by meeshbhoombah
+
 A hangman game created for a homework assignment
 for Make School's CS1 class.
 
@@ -9,6 +9,27 @@ same directory.
 """
 
 import random
+
+game_screen = ["""
+
+    Welcome to...    
+
+    ██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ███╗   ███╗ █████╗ ███╗   ██╗
+    ██║  ██║██╔══██╗████╗  ██║██╔════╝ ████╗ ████║██╔══██╗████╗  ██║
+    ███████║███████║██╔██╗ ██║██║  ███╗██╔████╔██║███████║██╔██╗ ██║
+    ██╔══██║██╔══██║██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║
+    ██║  ██║██║  ██║██║ ╚████║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║
+    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+                                                                
+    And yes, I do know this is ridculous overkill. Press Enter to play 
+    the damn game. (ENTER)
+
+""",
+"""
+    MENU:
+    (F) to play single player (G) to play two player (H) to read the rules
+
+"""]
 
 class Game(object):
     number_of_guesses = 6
@@ -19,12 +40,15 @@ class Game(object):
         self.hint_li = []
 
     def set_secret_li():
-
+    ''' Split the secret word into a list of letters '''
+    
     def set_guess_li():
+    ''' Create a list for '''
     
     def check_character():
         
     def get_guess_li():
         
     def decrement_guesses():
+
 
