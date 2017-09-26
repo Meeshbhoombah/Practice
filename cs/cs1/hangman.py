5be5853e735053e377ad410149c6b694bf3b44f0
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-26 10:33:33 -0700

    Added function to loadWord from 'words.txt'

--- cs/cs1/hangman.py
+++ cs/cs1/hangman.py
@@ -29,26 +29,55 @@ game_screen = ["""
     MENU:
     (F) to play single player (G) to play two player (H) to read the rules
 
+""",
+"""
+    Rules:
+""",
+"""
+    1
+""",
+"""
+    2
+""",
+"""
+    3
+""",
+"""
+    4
+""",
+"""
+    5
+""",
+"""
+    game over
 """]
 
+# Loads word from file "words.txt" located in the same directory
+def loadWord():
+   f = open('words.txt', 'r')
+   wordsList = f.readlines()
+   f.close()
+
+   wordsList = wordsList[0].split(' ')
+   secretWord = random.choice(wordsList)
+   return secretWord
+
+###### GAME CLASS ######
+
 class Game(object):
-    number_of_guesses = 6
+    incorrect_guesses_allowed = 6
 
     def __init__(self, ):
         self.secret_word = secret_word
         self.secret_li = []
         self.hint_li = []
 
-    def set_secret_li():
-    ''' Split the secret word into a list of letters '''
-    
-    def set_guess_li():
-    ''' Create a list for '''
-    
-    def check_character():
-        
-    def get_guess_li():
-        
-    def decrement_guesses():
+###### GAME LOGIC #####
+
+print(game_screen[0])
+try: 
+    input()
+except SyntaxError:
+    pass
 
 
