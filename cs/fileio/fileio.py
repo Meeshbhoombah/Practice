4134516e2096dabed1ade24a0ab4588be660c595
Rohan Mishra <rohanmishra2017@gmail.com>
2017-10-13 10:46:13 -0700

    Learned fileio.py

--- cs/fileio/fileio.py
+++ cs/fileio/fileio.py
@@ -4,13 +4,11 @@ with open('sales_data.txt') as f:
     cleaned_data = clean_up(f)
     cleanded_data[:4]
 
- 
 """
 -*- .split vs .replace -*-
 - replace creates strings
 - split creates a list full of strings
 """
-
 def clean_up(raw_data):
     cleaned_lines = []
 
@@ -20,10 +18,25 @@ def clean_up(raw_data):
 
         # split the line into a list
         cleaned_line = cleaned_line.split('\t')
-
+        
         cleaned_line[3] = float(cleaned_line[3]) 
-
         cleaned_lines.append(cleaned_line)
 
     return cleaned_lines
+
+all_philly_sales = [i for i in cleaned_data if i[0] == 'Philadelphia']
+
+"""
+-*- .sum -*-
+- the sum of all values in a list
+"""
+total_sales = sum(i[3] for i in cleaned_data)
+print(total_sales)
+
+"""
+-*- integer overflow v.s underflow -*-
+- ints = most precise data type because you risk over/under-flow
+  w/ other data types
+- floats are calculated differently
+"""
 
