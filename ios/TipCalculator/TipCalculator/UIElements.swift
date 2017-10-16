f92e638a13b4c9f7a404aa8a2f0b3920b710170f
Rohan Mishra <rohanmishra2017@gmail.com>
2017-10-15 22:30:19 -0700

    Added top bar menu on tip calculator

---
+++ ios/TipCalculator/TipCalculator/UIElements.swift
@@ -0,0 +1,19 @@
+//
+//  UIElements.swift
+//  TipCalculator
+//
+//  Created by Rohan Mishra on 10/15/17.
+//  Copyright © 2017 Augment. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UIElements {
+    
+    func divider(anchor: UIView) {
+        let line = UIView()
+        
+    }
+    
+}
--- ios/TipCalculator/TipCalculator/ViewController.swift
+++ ios/TipCalculator/TipCalculator/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum service {
+        case poor
+        case average
+        case great
+        case beautiful
+    }
 
+    @IBAction func resetCalculator(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
-
